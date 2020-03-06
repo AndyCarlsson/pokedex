082ce32404f0634be5055f49aec8cb4fb096f031
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-06 07:03:21 +0100

    Started with MainScreen (add view to name) and added testdata

---
+++ Pokedex_xf/Pokedex_xf/Pokedex_xf/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Pokedex_xf.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pokedex_xf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainScreen();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Pokedex_xf/Pokedex_xf/Pokedex_xf/Views/MainScreen.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Pokedex_xf.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainScreen : ContentPage
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+            pokeCollectionBinding.ItemsSource = pokeCollection;
+        }
+
+        ObservableCollection<Pokemon> pokeCollection = new ObservableCollection<Pokemon>() 
+        {
+            new Pokemon { Id = 1, Name = "Bulbasaur" }
+        };
+
+
+
+    }
+
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
