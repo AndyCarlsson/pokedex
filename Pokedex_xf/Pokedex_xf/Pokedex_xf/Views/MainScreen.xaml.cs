7ffdf690d84ddeaf049e58d5dc33fb539596627c
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-08 17:27:21 +0100

    Created a PokemonDetailPage

--- Pokedex_xf/Pokedex_xf/Pokedex_xf/Views/MainScreen.xaml.cs
+++ Pokedex_xf/Pokedex_xf/Pokedex_xf/Views/MainScreen.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,22 +12,33 @@ namespace Pokedex_xf.Views
         public MainScreen()
         {
             InitializeComponent();
-            pokeCollectionBinding.ItemsSource = pokeCollection;
+            pokeCollectionBinding.ItemsSource = pokeCollection;           
         }
 
         ObservableCollection<Pokemon> pokeCollection = new ObservableCollection<Pokemon>()
         {
-            new Pokemon { Id = 1, Name = "Bulbasaur" },
-            new Pokemon { Id = 2, Name = "Pikachu" },
-            new Pokemon { Id = 10, Name = "Kakuna" }
+            new Pokemon { Id = 1, Name = "Bulbasaur", Type= "Grass" },
+            new Pokemon { Id = 2, Name = "Pikachu", Type = "Electric"},
+            new Pokemon { Id = 10, Name = "Kakuna", Type = "Bug"}
         };
 
+        public void changeBgColor()
+        {
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+          
+        }
     }
+
+
 
     public class Pokemon
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Type { get; set; }
     }
-
 }
