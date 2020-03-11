a5b4848383e295fb071f44cf43d61059e41a1054
AndyCarlsson <andreas95carlsson@gmail.com>
2020-03-11 19:35:30 +0100

    Continue design of detailpage

--- Pokedex_xf/Pokedex_xf/Pokedex_xf/Views/PokemonDetailPage.xaml.cs
+++ Pokedex_xf/Pokedex_xf/Pokedex_xf/Views/PokemonDetailPage.xaml.cs
@@ -17,6 +17,7 @@ namespace Pokedex_xf.Views
         {
             InitializeComponent();
             NameLabel.Text = pokemon.Name;
+            TypeLabel.Text = pokemon.Type;
         }
     }
 }
